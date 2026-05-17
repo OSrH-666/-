30b93613dfffa493ab0db1f90eb40e4ec64801f7
leegus3500056753-png <leegus3500056753@gmail.com>
2026-05-17 22:44:03 +0800

    feat: 添加密码生成工具

--- backend/GeneratePassword.java
+++ backend/GeneratePassword.java
@@ -3,10 +3,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GeneratePassword {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String password = "123456";
-        String encoded = encoder.encode(password);
-        System.out.println("Password: " + password);
-        System.out.println("Encoded: " + encoded);
-        System.out.println("Matches: " + encoder.matches(password, encoded));
+        String rawPassword = "123456";
+        String encodedPassword = encoder.encode(rawPassword);
+        System.out.println("Encoded password: " + encodedPassword);
     }
 }
